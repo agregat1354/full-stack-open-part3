6a96a239f192a58873d723a0bf772e1f69c21179
agregat1354 <icollectreports@gmail.com>
2023-07-21 11:21:41 +0200

    backendbuild

--- index.js
+++ index.js
@@ -82,5 +82,5 @@ app.post('/api/persons', (request, response) => {
     response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`server listening on port ${PORT}`))
